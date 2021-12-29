7c5682e874fb986e0c4bdb5c68569fc7c919dd10
Arif Bryan <arief372@gmail.com>
2021-12-29 19:51:55 +0700

    Initial commit

---
+++ Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Interrupts.c
+
+DS3231.cpp
+
+I2CPeriph.cpp
+
+main.cpp
+
+MAX7219_7Seg.cpp
+
+SPIPeriph.cpp
+
---
+++ DigitalDeskClock_v2/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * DigitalDeskClock_v2.cpp
+ *
+ * Created: 2021-12-29 7:49:03 PM
+ * Author : Arif Bryan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void){
+    while(1){
+    }
+}
+
